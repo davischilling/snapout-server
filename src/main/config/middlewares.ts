08e17a089c40ffe1ace5a2d12a16897ddb63656b
Davi Schilling <davischilling.dev@gmail.com>
2021-10-02 01:57:27 +0200

    add origin to cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -2,7 +2,9 @@ import { json, Express } from 'express'
 import cors from 'cors'
 
 export const setupMiddlewares = (app: Express): void => {
-  app.use(cors())
+  app.use(cors({
+    origin: process.env.CLIENT_URL
+  }))
   app.use(json())
   app.use((req, res, next) => {
     res.type('json')
--- src/main/index.ts
+++ src/main/index.ts
@@ -7,7 +7,7 @@ import mongoose from 'mongoose'
 const start = async (): Promise<void> => {
   console.log('Starting snapout server...')
 
-  const processEnv = env(['TOKEN_EXPIRATION', 'NODE_ENV', 'PORT', 'DB_NAME', 'MONGO_URI', 'JWT_SECRET'])
+  const processEnv = env(['TOKEN_EXPIRATION', 'NODE_ENV', 'PORT', 'DB_NAME', 'MONGO_URI', 'JWT_SECRET', 'CLIENT_URL'])
   if (processEnv instanceof Error) {
     throw processEnv
   }
