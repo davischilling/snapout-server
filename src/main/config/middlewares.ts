28044a8eb296c7c46cfc0ca20c3962e947a1e6b2
Davi Schilling <davischilling.dev@gmail.com>
2021-11-12 16:53:23 +0100

    feat: setup cors middleware to allow multiple clist hosts

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,19 @@
 import { json, Express } from 'express'
 import cors from 'cors'
 
+const whitelist = process.env.CLIENT_URL !== null ? (process.env.CLIENT_URL as string).split(' ') : ''
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (origin === undefined || whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 export const setupMiddlewares = (app: Express): void => {
-  app.use(cors({
-    origin: process.env.CLIENT_URL
-  }))
+  app.use(cors(corsOptions))
   app.use(json())
   app.use((req, res, next) => {
     res.type('json')
